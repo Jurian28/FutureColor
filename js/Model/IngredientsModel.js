2b6220b3e10b142dba2bc42b6f389e4004e1e1f7
EthanvdGlas <164499757+EthanvdGlas@users.noreply.github.com>
2025-06-15 17:19:30 +0200

    hele kleine fix

--- js/Model/IngredientsModel.js
+++ js/Model/IngredientsModel.js
@@ -34,7 +34,6 @@ export class IngredientsModel {
 
     addIngredientToPot(potId, ingredientId) {
         let ingredient = this.find(ingredientId);
-        ingredient.addToPot(potId);
         ingredient.inPotId = potId;
         this.saveIngredients();
     }
